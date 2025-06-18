2458214009be5f3884e668f892b2ea54e0ca8a2c
cyuzuzogermain <c.germain@alustudent.com>
2025-06-18 13:44:12 -0700

    #

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
 
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
