4cf890a957152fc52b0b6fad5dc5de3da8c7f612
cyuzuzogermain <c.germain@alustudent.com>
2025-06-29 23:49:45 -0700

    #

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@
 def safe_print_integer(value):
     try:
         if isdigit(value):
-            print("{:d}.format()", value)
+            print("{:d}.format(value)")
             return True
     except IndexError:
         return False
