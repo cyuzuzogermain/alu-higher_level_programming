943bce254eb6ca9d9a79474bdf598ef6a443da7e
cyuzuzogermain <c.germain@alustudent.com>
2025-06-30 00:51:50 -0700

    #

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except ZeroDivisionEror:
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
