886814967c81c4fe9c45758ed422cb9c53ff2ef7
cyuzuzogermain <c.germain@alustudent.com>
2025-07-01 02:12:45 -0700

    #

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,17 @@
-#!/usr/bin/python3
+"""
+Module: shapes
+
+This module defines geometric shape classes.
+Currently, it includes an empty Square class for future implementation.
+"""
 
 class Square:
-    #This is a class containing shapes like square
-    def __init__(self):
-        #This is the function that returns the values of this class
+        """
+            A class to represent a square.
+
+                This is an empty class intended as a placeholder for future development.
+                    Methods and attributes such as side length, area calculation,
+                        or perimeter will be added later.
+                            """
+                                pass
+                            
