ebb1368428db1725e7842c0d7cd9cb541f7388e3
cyuzuzogermain <c.germain@alustudent.com>
2025-07-03 09:16:54 -0700

    #

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,11 +8,13 @@ This module defines a basic Square class.
 It currently contains no attributes or methods.
 Used for demonstrating class creation and object inspection.
 """
+
+
 class Square:
         """
         Represents a square.
         This is an empty class definition for a square.
-        It is used to show basic class structure in Python 
+        It is used to show basic class structure in Python
         without functionality.
         """
         pass
