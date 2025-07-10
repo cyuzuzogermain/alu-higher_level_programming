29873035e200974e0c6c396f6eb3298c636f805a
cyuzuzogermain <c.germain@alustudent.com>
2025-07-10 04:42:42 -0700

    #

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
+"""
+A function that return the list of available
+attrubutes and methods of an object
+"""
 def lookup(obj):
+    """
+    function body block
+    """
     return []
