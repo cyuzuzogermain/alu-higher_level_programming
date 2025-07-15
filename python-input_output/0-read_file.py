1c52cb8051eadbb5b95412150c82f053116d9775
cyuzuzogermain <c.germain@alustudent.com>
2025-07-15 00:29:04 -0700

    #

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+A function to read files and print them to
+stdout
+"""
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
