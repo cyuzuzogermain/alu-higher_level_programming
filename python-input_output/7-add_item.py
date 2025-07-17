d9f6ec399cd0f5ac4b4f757f2c4ca078cfcd649c
cyuzuzogermain <c.germain@alustudent.com>
2025-07-17 12:41:39 -0700

    #

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,14 +8,20 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+"""
+a function
+"""
 file = "add_item.json"
 
 try:
+    """ function """
     arg_list = load_from_json_file(f)
 except FileNotFoundError:
+    """ function """
     arg_list = list()
 
 for arg in argv[1:]:
+    """ function """
     arg_list.append(arg)
 
 save_to_json_file(arg_list, f)
