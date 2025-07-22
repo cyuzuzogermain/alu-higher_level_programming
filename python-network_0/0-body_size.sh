c02ce98c08650ad4c023d91b8b4717065f457814
cyuzuzogermain <c.germain@alustudent.com>
2025-07-22 10:59:51 +0000

    #

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL and displays the size in bytes of a 200 OK response body
-curl -sL -w "%{http_code}" "$1" -o body.tmp | grep -q 200 && wc -c < body.tmp || echo 0
+# Sends a request to a URL and displays the size of the response body in bytes
+curl -s "$1" | wc -c
