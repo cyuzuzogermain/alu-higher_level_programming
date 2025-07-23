066c505604c0348eb4ad10cf4f11ccf984767004
cyuzuzogermain <c.germain@alustudent.com>
2025-07-23 11:07:13 -0700

    #

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get response body size in bytes using curl for a given URL
-curl -s -o /dev/null -w "GET / => \"$(curl -s $1)\"\n" $1
+# Sends a request to a URL and displays the size of the response body in bytes
+curl -s "$1" | wc -c
