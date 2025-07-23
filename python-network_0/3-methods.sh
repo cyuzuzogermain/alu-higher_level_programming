ac5b578ba016e4cf25330493997dddc244f81706
cyuzuzogermain <c.germain@alustudent.com>
2025-07-23 13:17:47 -0700

    #

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends an OPTIONS request and extracts the Allow header
-curl -sI -X OPTIONS "$1" | grep -i ^Allow:
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
