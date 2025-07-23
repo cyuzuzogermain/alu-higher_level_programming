d37c0ab6cfdd1341196d051bea33e30eab86aea2
cyuzuzogermain <c.germain@alustudent.com>
2025-07-23 13:29:09 -0700

    #

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-"""
-A script that
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
-"""
-
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-if url.startswith('https://'):
-    url = 'https://alu-intranet.hbtn.io/status'
+url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == '__main__':
-    with urllib.request.urlopen(url) as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
