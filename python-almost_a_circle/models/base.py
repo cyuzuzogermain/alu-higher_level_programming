b74c0a5e958e7cfe1d5307b1d7e92c645cd83748
cyuzuzogermain <c.germain@alustudent.com>
2025-07-24 14:38:48 -0700

    #

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Base module: contains the Base class which will be the foundation
+for all other classes in this project.
+"""
+
+class Base:
+    """The Base class to manage `id` attribute for all future classes."""
+
+    __nb_objects = 0  # private class attribute
+
+    def __init__(self, id=None):
+        """
+        Initialize the base instance.
+
+        Args:
+            id (int, optional): The id to assign. If None, auto-increment is used.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
