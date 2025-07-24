c328d1da26a93457e0e93f71cf1d98b782fee05a
cyuzuzogermain <c.germain@alustudent.com>
2025-07-24 15:24:15 -0700

    #

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,5 +108,5 @@ class Rectangle(Base):
         Format:
             [Rectangle] (<id>) <x>/<y> - <width>/<height>
         """
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
-
+        return f"[Rectangle] ({self.id}) 
+        {self.x}/{self.y} - {self.width}/{self.height}"
